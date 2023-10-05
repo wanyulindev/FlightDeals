0a8196ba388388f3848cec6471472170b6b03f37
wanyulindev <wanyulin052@gmail.com>
2023-10-05 15:58:39 -0700

    Reading data from Google sheet - done.

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,37 @@
+import requests
+import os
+from pprint import pprint
+
+# AUTH = os.environ.get("SHEETY_AUTH")
+# ENDPOINT = os.environ.get("SHEETY_URL")
+# HEADERS = {
+#             "Authorization": AUTH
+#         }
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.auth = os.environ.get("SHEETY_AUTH")
+        self.url = os.environ.get("SHEETY_URL")
+        self.headers = {
+            "Authorization": self.auth
+        }
+        # self.get_response = requests.get(self.url, headers=self.headers)
+        # self.get_data()
+        self.data = {}
+
+    def get_data(self):
+        # get_response = requests.get(ENDPOINT, headers=HEADERS)
+        get_response = requests.get(self.url, headers=self.headers)
+        self.data = get_response.json()
+        return pprint(self.data)
+        # return self.get_response.text
+        # print(self.get_response.json())
+
+        
+
+
+
+
+# It's time to review OOP to make it more clear idea to my knowledge!
+# (151)
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+
+data_manager = DataManager()
+print(data_manager.get_data())
+# sheet_data = data_manager.get_data()
+# print(sheet_data)
+
+
+
+
+# Test it out why can't print out: (FIXED!)
+# import os
+# import requests
+#
+# # AUTH = os.environ.get("SHEETY_AUTH")
+# # ENDPOINT = os.environ.get("SHEETY_URL")
+# # HEADERS = {
+# #             "Authorization": AUTH
+# #         }
+#
+# get_response = requests.get("https://api.sheety.co/792a621e9a95f9932e7a7e96c1680077/workoutTracking/workouts")
+# print(get_response.text)
+# # data = get_response.json()
+# # print(data)
